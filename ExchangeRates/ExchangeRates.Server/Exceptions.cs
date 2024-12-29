639cacbaf7c6b10c60934275993a2cca2117a64d
Dominic Cronin <dominic@cronin.tech>
2024-12-29 18:49:21 +0100

    Suppress warnings about namespaces

---
+++ ExchangeRates/ExchangeRates.Server/Exceptions.cs
@@ -0,0 +1,39 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace ExchangeRates.Server.Exceptions;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+
+[Serializable]
+public class ValidationException : Exception
+{
+	public ValidationException() { }
+	public ValidationException(string message) : base(message) { }
+	public ValidationException(string message, Exception inner) : base(message, inner) { }
+}
+
+
+[Serializable]
+public class UpstreamServiceException : Exception
+{
+	public UpstreamServiceException() { }
+	public UpstreamServiceException(string message) : base(message) { }
+	public UpstreamServiceException(string message, Exception inner) : base(message, inner) { }
+
+	public UpstreamServiceException(string message, int statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public int StatusCode { get; init; }
+}
+
+[Serializable]
+public class UpstreamBadRequestException : UpstreamServiceException
+{
+	public UpstreamBadRequestException() { }
+	public UpstreamBadRequestException(string message) : base(message) {
+		StatusCode = StatusCodes.Status400BadRequest;
+	}
+	public UpstreamBadRequestException(string message, Exception inner) : base(message, inner) { }
+
+}
