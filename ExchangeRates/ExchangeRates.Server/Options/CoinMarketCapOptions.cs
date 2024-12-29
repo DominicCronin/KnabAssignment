35bf50d4e3bfd04b300a32682e96f8fb26270440
Dominic Cronin <dominic@cronin.tech>
2024-12-29 18:52:20 +0100

    Add config

---
+++ ExchangeRates/ExchangeRates.Server/Options/CoinMarketCapOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ExchangeRates.Server.Options
+{
+    public class CoinMarketCapOptions
+    {
+        public const string Section = "CoinMarketCap";
+        public string ApiKey { get; set; } = String.Empty;
+        public string BaseUrl { get; set; } = string.Empty;
+        public string[] CurrencySymbols { get; set; } = [];
+    }
+}
