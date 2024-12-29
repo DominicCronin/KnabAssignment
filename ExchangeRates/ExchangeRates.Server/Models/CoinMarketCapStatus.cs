0a27a5a0b7c37217cbb32209ec39fbd94c872e5a
Dominic Cronin <dominic@cronin.tech>
2024-12-29 18:51:37 +0100

    Models for CoinMarketCap client

---
+++ ExchangeRates/ExchangeRates.Server/Models/CoinMarketCapStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace ExchangeRates.Server.Models
+{
+    public class CoinMarketCapStatus
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public int ErrorCode { get; set; }
+        public string? ErrorMessage { get; set; }
+        public int Elapsed { get; set; }
+        public int CreditCount { get; set; }
+        public string? Notice { get; set; }
+    }
+}
---
+++ ExchangeRates/ExchangeRates.Server/Models/IdMapResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace ExchangeRates.Server.Models
+{
+    public class IdMapResponse
+    {
+        public required CoinMarketCapStatus Status { get; set; }
+        public required IdMapResponseData[] Data { get; set; }
+
+
+    }
+
+    public class IdMapResponseData
+    {
+        public int Id { get; set; }
+
+        public int Rank { get; set; }
+        public string? Name { get; set; }
+
+        public string? Symbol { get; set; }
+        public string? Slug { get; set; }
+        public DateTimeOffset FirstHistoricalData { get; set; }
+        public DateTimeOffset LastHistoricalData { get; set; }
+    }
+}
